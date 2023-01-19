f9426fa10261089efa9fc5fd4c361f2f241f400d
Blessing Udiong <udiongblessing@gmail.com>
2023-01-19 21:03:55 +0000

    Automate config file using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,7 @@
 # This puppet manifest makes changes to our configuration file
 
-file_line {'ssh_config':
-  path  => '~/.ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '*IdentityFile ~/.ssh/id_rsa',
-}
 
-file_line {'ssh_config':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'PasswordAuthenticaion no',
-  match => '*PasswordAuthentication Yes',
+exec {'Configure file':
+  command => '/bin/echo -e "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
+  path    => '/etc/ssh/ssh_config',
 }
