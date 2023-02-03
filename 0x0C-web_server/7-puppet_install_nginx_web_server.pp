5b4a9d1697036dc26c3cfe4810d057103f7ba55a
Blessing Udiong <udiongblessing@gmail.com>
2023-02-03 10:59:59 +0000

    Fix bugs

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,7 +3,7 @@
 exec {'Config Ubuntu Machine':
   command  => 'sudo apt-get update;
 		sudo apt-get install nginx -y;
-		echo "Hello World!" | sudo tee index.html;
+		echo "Hello World!" | sudo tee /var/www/html/index.html;
 		new_config="\ \n\tlocation \/redirect_me \{\ \n\t\treturn 301 \$scheme\:\/\/\$http_host\;\ \n\t\}\ \n";
 		sudo sed -i "/server_name _;/a $new_config" /etc/nginx/sites-enabled/default;
 		sudo service nginx restart',
